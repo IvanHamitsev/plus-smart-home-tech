7757599b0e6815e93b0e45782f99248c55cd91df
Ivan Hamitsev <hamitsev-ie@yandex.ru>
2025-05-27 19:06:32 +0300

    Update module infra/config-server with shopping-cart and warehouse support

--- infra/discovery-server/src/main/java/ru/yandex/practicum/DiscoveryServer.java
+++ infra/discovery-server/src/main/java/ru/yandex/practicum/DiscoveryServer.java
@@ -1,4 +1,4 @@
-package commerce.shopping_cart;
+package ru.yandex.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
